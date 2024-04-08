e22d5048b7ac32699c2fa5e02da7791fb03bfa49
Maleah <111613370+xleahw@users.noreply.github.com>
2024-04-08 16:22:45 -0400

    Add files via upload

---
+++ components/Discover.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Discover = () => {
+  return (
+    <div className="discover">
+      <div className="circle"></div>
+      <h2>DISCOVER</h2>
+    </div>
+  );
+};
+
+export default Discover;
