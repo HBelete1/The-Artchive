579ba3f901c34a2914fbbd9cfa7fa6528fa9664a
sophia-rhoads <119001471+sophia-rhoads@users.noreply.github.com>
2024-04-08 21:26:11 -0400

    Add files via upload

---
+++ src/app/portfolio/page.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import FolioHeader from './portfolio_view/FolioHeader';
+import Container from './portfolio_view/Container';
+
+function App() {
+  return (
+    <div>
+      <FolioHeader />
+      <Container />
+    </div>
+  )
+}
+
+export default App;
