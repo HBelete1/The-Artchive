df2c3d33da8356b07f6dd0f316684b61e834426d
sophia-rhoads <119001471+sophia-rhoads@users.noreply.github.com>
2024-04-19 09:06:11 -0400

    Add files via upload

---
+++ artchive_server/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8085;
+const mongoose = require('mongoose');
+const cors = require('cors');
+const items = require('./routes/api/items');
+
+app.use(cors({origin: true, credentials: true}));
+app.use('/api/items', items);
+app.get('/', (req, res) => res.send('Hello world'));
+
+const conn_str = 'mongodb+srv://sdr92017:sockstiger@artchivecluster.xswurje.mongodb.net/?retryWrites=true&w=majority&appName=ArtchiveCluster';
+mongoose.set('strictQuery', false);
+mongoose.connect(conn_str).then(() => {
+    app.listen(port, () => console.log(`Server running on port ${port}`));
+    console.log(`MongoDB Connection Suceeded...`);
+})
+.catch(err => {
+    console.log(`Error in DB Connection ${err}`);
+});
